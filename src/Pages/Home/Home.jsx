ffc040901201830ae9336f57af1b9137399ed471
Kj-RahiL <rahiilarham@gmail.com>
2023-10-09 21:33:14 +0600

    update responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -19,14 +19,14 @@ const Home = () => {
             <Banner></Banner>
             <div className="bg-[#f7f7f7] pt-8">
 
-                <div data-aos="fade-up"  data-aos-duration="3000"><Service></Service></div>
-                <div data-aos="fade-left" className="mt-20">
+                <div data-aos="zoom-in"  data-aos-duration="3000"><Service></Service></div>
+                <div data-aos="zoom-in-up" className="mt-20">
 
                     <h3 className="my-5 text-4xl font-bold  text-center ">Our Events <span className="text-[#fd2d2d]">Gallery</span> </h3>
                     <p className="text-center text-gray-500 text-xl font-medium">We make your events smart & impactful by personalised event management services.</p>
 
                     <Gallary></Gallary>
-                    <div data-aos="flip-right"  data-aos-duration="3000" className="flex flex-col md:flex-row justify-center gap-8 items-center pb-4">
+                    <div data-aos="zoom-in-down"  data-aos-duration="3000" className="flex flex-col md:flex-row justify-center gap-8 items-center pb-4">
                         <h2 className="text-xl font-medium">See Our Full Gallery of Events!</h2>
                         <button className="btn rounded-full bg-[#fd2d2d] hover:text-black text-white">Visit full Gallery</button>
                     </div>
